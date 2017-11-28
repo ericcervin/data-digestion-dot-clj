7563f55f8832dd34c6076a257e1a4a640907cfba
ericcervin <ericcervin@gmail.com>
2017-11-28 17:27:51 -0500

    added another report. added "points" field

--- src/data_digestion/destiny.clj
+++ src/data_digestion/destiny.clj
@@ -5,13 +5,16 @@
 (defn export-card-tsv [file mp]
   (with-open [writer (io/writer file)]
     (doseq [i mp]
-      (.write writer (str (get i "set_name") "\t" (str (get i "position") "\t" (get i "affiliation_name") "\t" (get i "faction_name") "\t" (get i "name") "\t" (get i "type_name") "\n"))))))
+      (.write writer (str (get i "set_name") "\t" (get i "position") "\t" (get i "affiliation_name") "\t" (get i "faction_name") "\t" (get i "name") "\t" (get i "type_name") "\t" (get i "points") "\n")))))
         
 
 (defn -main []
   (let [card-json (slurp "https://swdestinydb.com/api/public/cards/")
         card-map  (json/read-str card-json)
-        villain-command-cards (filter #(and (= (get % "affiliation_name") "Villain") (= (get % "faction_name") "Command")) card-map)]
+        villain-command-cards (filter #(and (= (get % "affiliation_name") "Villain") (= (get % "faction_name") "Command")) card-map)
+        villain-command-compatible-cards (filter #(or (and (= (get % "affiliation_name") "Villain") (= (get % "faction_name") "General"))
+                                                      (and (= (get % "affiliation_name") "Neutral") (= (get % "faction_name") "General") (not (= (get % "type_name") "Battlefield")))
+                                                      (and (= (get % "affiliation_name") "Neutral") (= (get % "faction_name") "Command"))) card-map)]
     
     ;;export all json
     (spit "resources/all_cards.json" card-json)
@@ -22,9 +25,10 @@
     ;;write reports
     (export-card-tsv "resources/card_list_all.tsv" card-map)
     (export-card-tsv "resources/card_list_villian_command.tsv" villain-command-cards)
+    (export-card-tsv "resources/card_list_villian_command_compatible.tsv" villain-command-compatible-cards)))
     
 
-    ))
+    
         
         
 
