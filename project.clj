51383a2a3355d30b1a87f30eca973f9081c8f9ad
ericcervin <ericcervin@gmail.com>
2017-12-02 23:38:42 -0500

    added jdbc and sqlite dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject data_digestion "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.xerial/sqlite-jdbc "3.20.0"]])
