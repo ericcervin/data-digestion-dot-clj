178730f93b377d31999c21653620c568a96b0ff3
ericcervin <ericcervin@gmail.com>
2018-01-12 23:56:10 -0500

    merged two destiny tsv reports into card_list_villain_command_and_compatible.tsv

--- src/data_digestion/destiny.clj
+++ src/data_digestion/destiny.clj
@@ -62,10 +62,9 @@
   (let [card-json (slurp "https://swdestinydb.com/api/public/cards/")
         card-map  (json/read-str card-json)
         card-map  (map assoc-extended-points-fields card-map)
-        villain-command-cards (filter #(and (= (get % "affiliation_name") "Villain") (= (get % "faction_name") "Command")) card-map)
-        villain-command-compatible-cards (filter #(or (and (= (get % "affiliation_name") "Villain") (= (get % "faction_name") "General"))
-                                                      (and (= (get % "affiliation_name") "Neutral") (= (get % "faction_name") "General") (not (= (get % "type_name") "Battlefield")))
-                                                      (and (= (get % "affiliation_name") "Neutral") (= (get % "faction_name") "Command"))) card-map)]
+        villain-command-and-compatible-cards (filter #(and (or (= (get % "affiliation_name") "Villain") (= (get % "affiliation_name") "Neutral"))
+                                                           (or (= (get % "faction_name") "Command") (= (get % "faction_name") "General")))
+                                                      card-map)]
     
     ;;export all json
     (spit "resources/destiny/OUT/all_cards.json" card-json)
@@ -78,8 +77,8 @@
     
     ;;write reports
     (export-card-tsv "resources/destiny/OUT/card_list_all.tsv" card-map)
-    (export-card-tsv "resources/destiny/OUT/card_list_villian_command.tsv" villain-command-cards)
-    (export-card-tsv "resources/destiny/OUT/card_list_villian_command_compatible.tsv" villain-command-compatible-cards)
+    (export-card-tsv "resources/destiny/OUT/card_list_villain_command_and_compatible.tsv" villain-command-and-compatible-cards)
+    
     
     ;;drop card table
     (drop-table! "card")
