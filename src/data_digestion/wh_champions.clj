fa7c1cf5811fcef274385eebaea437346d84fec5
ericcervin <ericcervin@gmail.com>
2018-11-23 22:29:36 -0500

    added class and rarity to wh_champions tsv exports

--- src/data_digestion/wh_champions.clj
+++ src/data_digestion/wh_champions.clj
@@ -7,19 +7,21 @@
 
 (defn export-card-tsv [file mp]
   (with-open [writer (io/writer file)]
-    (.write writer (str (clojure.string/join "\t" ["Number" "Faction" "Category"  "Name"]) "\n"))
+    (.write writer (str (clojure.string/join "\t" ["Number" "Faction" "Category" "Class" "Name" "Rarity"]) "\n"))
     (doseq [i mp]
       (let [c-number (get-in i [:_source :collectorNumber])
             c-faction (get-in i [:_source :alliance])
             c-category (get-in i [:_source :category :en])
-            c-name (get-in i [:_source :name])]
-        (.write writer (str (clojure.string/join "\t" [c-number c-faction c-category c-name]) "\n"))))))        
+            c-class (get-in i [:_source :class :en])
+            c-name (get-in i [:_source :name])
+            c-rarity (get-in i [:_source :rarity])]
+        (.write writer (str (clojure.string/join "\t" [c-number c-faction c-category c-class c-name c-rarity]) "\n"))))))        
 
 (defn -main []
   (let [c (sp/client {:hosts ["https://carddatabase.warhammerchampions.com"]})
         response  (sp/request c {:url "/warhammer-cards/_search"              
                                  :method :post
-                                 :body {:size 100}})
+                                 :body {:size 1000}})
         response-body (:body response)
         card-count (get-in response-body [:hits :total])
         all-cards (get-in response-body [:hits :hits])
