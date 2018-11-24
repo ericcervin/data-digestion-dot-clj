ce0419d5ff801fefcd8bd4c21318dbbc5e570d8c
ericcervin <ericcervin@gmail.com>
2018-11-24 00:21:15 -0500

    counts/subtotals at print time in wh_champions.clj

--- src/data_digestion/wh_champions.clj
+++ src/data_digestion/wh_champions.clj
@@ -7,15 +7,15 @@
 
 (defn export-card-tsv [file mp]
   (with-open [writer (io/writer file)]
-    (.write writer (str (clojure.string/join "\t" ["Number" "Faction" "Category" "Class" "Name" "Rarity"]) "\n"))
+    (.write writer (str (clojure.string/join "\t" ["Number" "Alliance" "Category" "Class" "Name" "Rarity"]) "\n"))
     (doseq [i mp]
       (let [c-number (get-in i [:_source :collectorNumber])
-            c-faction (get-in i [:_source :alliance])
+            c-alliance (get-in i [:_source :alliance])
             c-category (get-in i [:_source :category :en])
             c-class (get-in i [:_source :class :en])
             c-name (get-in i [:_source :name])
             c-rarity (get-in i [:_source :rarity])]
-        (.write writer (str (clojure.string/join "\t" [c-number c-faction c-category c-class c-name c-rarity]) "\n"))))))        
+        (.write writer (str (clojure.string/join "\t" [c-number c-alliance c-category c-class c-name c-rarity]) "\n"))))))        
 
 (defn -main []
   (let [c (sp/client {:hosts ["https://carddatabase.warhammerchampions.com"]})
@@ -26,16 +26,23 @@
         card-count (get-in response-body [:hits :total])
         all-cards (get-in response-body [:hits :hits])
         death-cards (filter #(= (get-in % [:_source :alliance]) "Death") all-cards)]
-        ;;all-cards (mapv #(get-in % [:_source :alliance]) death-list)]
-    (do
+        
+      ;;write counts of subsets (can compare with totals documented on Warhammer Champions)
+     (println (map #(str (first %) " " (count (last %))) (group-by #(get-in % [:_source :alliance]) all-cards)))
+     (println (map #(str (first %) " " (count (last %))) (group-by #(get-in % [:_source :rarity]) all-cards)))
+     (println (map #(str (first %) " " (count (last %))) (group-by #(get-in % [:_source :category :en]) all-cards)))
+    
+    
+     ;;some more subtotals
+     (println (sort (map #(str (first %) "\t" (count (last %)) ",") (group-by #(str (get-in % [:_source :alliance]) "-"(get-in % [:_source :rarity])) all-cards))))
+     (println (sort (map #(str (first %) "\t" (count (last %)) ",") (group-by #(str (get-in % [:_source :alliance]) "-"(get-in % [:_source :category :en])) all-cards))))
+     (println (sort (map #(str (first %) "\t" (count (last %)) ",") (group-by #(str (get-in % [:_source :category :en]) "-"(get-in % [:_source :rarity])) all-cards))))
+    
+       
      (spit "resources/wh_champions/OUT/all_cards.json" response-body)
      (export-card-tsv "resources/wh_champions/OUT/card_list_all.tsv" all-cards)
      (export-card-tsv "resources/wh_champions/OUT/card_list_death.tsv" death-cards) 
-     ;;(spit "resources/wh_champions/OUT/alliance_list.txt" alliance-list
-     ;;(spit "resources/wh_champions/OUT/death_list.txt" death-list
-     (println card-count)
-     (println (count death-cards)))))
+       
+     (println (str "Total Cards: "card-count))))
+     
       
-
-
-
